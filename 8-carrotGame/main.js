4182f7e29b70110f0c87a418b5f5b080041a86e0
Leegunhee777 <akanfldkdlel@ajou.ac.kr>
2022-01-21 02:02:51 +0900

    game start Timer setting

--- 8-carrotGame/main.js
+++ 8-carrotGame/main.js
@@ -1,13 +1,77 @@
 'use strict'
 const CARROT_SIZE = 80
+const CARROT_COUNT = 5
+const BUG_COUNT = 5
+const GAME_DURATION_SEC = 5
 const field = document.querySelector('.game__field')
 //getBoundingClientRect을 통해서
 //해당 DOM의 x,y, 위치좌표 및 width, heigt등을 알수있다.
 const fieldRect = field.getBoundingClientRect()
+const gameBtn = document.querySelector('.game__button')
+const gameTimer = document.querySelector('.game__timer')
+const gameScore = document.querySelector('.game__score')
 
+//게임의 시작유무
+let started = false
+//게임의 점수
+let score = 0
+//타이머
+let timer = undefined
+
+gameBtn.addEventListener('click', () => {
+  if (started) {
+    stopGame()
+  } else {
+    startGame()
+  }
+  started = !started
+})
+function startGame() {
+  initGame()
+  showStopButton()
+  showTimerAndScore()
+  startGameTimer()
+}
+function stopGame() {}
+
+function showStopButton() {
+  const icon = gameBtn.querySelector('.fa-play')
+  // fontawsome의 아이콘className을 활용하여 stop아이콘 이용할수있다
+  icon.classList.add('fa-stop')
+  icon.classList.remove('fa-play')
+}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = 'visible'
+  gameScore.style.visibility = 'visible'
+}
+
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC
+  updateTimerText(remainingTimeSec)
+
+  //1초마다 계속콜백을호출되게 하는함수 setInterval
+  //이것을 멈추려면 clearInterval 을 호출하면됨
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer)
+      return
+    }
+    updateTimerText(--remainingTimeSec)
+  }, 1000)
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
+  gameTimer.innerText = `${minutes}:${seconds}`
+}
 function initGame() {
-  addItem('carrot', 5, 'img/carrot.png')
-  addItem('bug', 5, 'img/bug.png')
+  //field 초기화
+  field.innerHTML = ``
+  gameScore.innerHTML = CARROT_COUNT
+  addItem('carrot', CARROT_COUNT, 'img/carrot.png')
+  addItem('bug', BUG_COUNT, 'img/bug.png')
 }
 
 function addItem(className, count, imgPath) {
@@ -37,4 +101,3 @@ function addItem(className, count, imgPath) {
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min
 }
-initGame()
